944921b51345c95db2a53daed63f54e9b1cc012e
Susumu Noda <susumunoda@gmail.com>
2023-09-08 20:12:01 -0700

    Apply dependency analysis Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,6 @@
 plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.dependencyAnalysis) apply true
 }
 true // Needed to make the Suppress annotation work for the plugins block
